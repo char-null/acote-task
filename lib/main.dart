71b7d231c335d8a3157e37d17fbfa09528311cc7
char-null <wool999999@gmail.com>
2024-08-16 23:25:28 +0900

    project init

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(391, 844),
+        builder: (context, child) {
+          return const MaterialApp();
+        });
+  }
+}
